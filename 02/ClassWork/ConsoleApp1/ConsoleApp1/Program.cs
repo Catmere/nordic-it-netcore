afa35cdfda67b47209dff8359480536240ebc829
Stas <stanislav_nazarov_97@mail.ru>
2019-12-29 14:20:30 +0300

    Added claswork2

---
+++ 02/ClassWork/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string str;
+			do
+			{
+				Console.WriteLine("Введите радиус");
+				float radius = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+				Console.WriteLine(radius * 2 * Math.PI);
+				Console.WriteLine("Введите repeat для повтора или Enter для завершения");
+				str = Console.ReadLine().ToLower();
+				
+			} while (str == "repeat");
+			
+			
+		}
+	}
+}
