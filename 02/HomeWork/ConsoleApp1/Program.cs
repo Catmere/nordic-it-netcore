871e85788607533e3ea5b4c5a59ad57757f3ad28
Kirill _Catmere_ Rogalskiy <catmere@yandex.ru>
2020-01-06 15:57:11 +0300

    Readabilty improved, changed method of cycling

--- 02/HomeWork/ConsoleApp1/Program.cs
+++ 02/HomeWork/ConsoleApp1/Program.cs
@@ -15,25 +15,48 @@ namespace ConsoleApp1
                 double y = double.Parse(Console.ReadLine());
                 string typeOfCalc;
                 double result = 0;
+                bool typeIsRight; //переменная для повтора цикла
                 do
                 {
-                    Console.WriteLine("Выберите действие: сложение, вычитание, умножение, деление, остаток от деления, возведение в степень или введите знак действия, если он существует");
+                    Console.WriteLine("Выберите действие (введите название или знак, если он существует):");
+                    Console.WriteLine(" сложение;");
+                    Console.WriteLine(" вычитание;");
+                    Console.WriteLine(" умножение;");
+                    Console.WriteLine(" деление;");
+                    Console.WriteLine(" остаток от деления;");
+                    Console.WriteLine(" возведение в степень.");
                     typeOfCalc = Console.ReadLine().ToLower();
+                    typeIsRight = true; //обновляем условие для цикла
                     if (typeOfCalc == "сложение" || typeOfCalc == "+")
-                    { result = x + y; }
+                    {
+                        result = x + y;
+                    }
                     else if (typeOfCalc == "вычитание" || typeOfCalc == "-")
-                    { result = x - y; }
+                    {
+                        result = x - y;
+                    }
                     else if (typeOfCalc == "умножение" || typeOfCalc == "*")
-                    { result = x * y; }
+                    {
+                        result = x * y;
+                    }
                     else if (typeOfCalc == "деление" || typeOfCalc == "/")
-                    { result = x / y; }
+                    {
+                        result = x / y;
+                    }
                     else if (typeOfCalc == "остаток от деления" || typeOfCalc == "остаток")
-                    { result = x % y; }
+                    {
+                        result = x % y;
+                    }
                     else if (typeOfCalc == "возведение в степень" || typeOfCalc == "^")
-                    { result = Math.Pow(x, y); }
+                    {
+                        result = Math.Pow(x, y);
+                    }
                     else
-                    { Console.WriteLine("Вы не ввели действие. Введите действие или его знак"); }
-                } while (!(typeOfCalc == "сложение" || typeOfCalc == "+" || typeOfCalc == "вычитание" || typeOfCalc == "-" || typeOfCalc == "умножение" || typeOfCalc == "*" || typeOfCalc == "деление" || typeOfCalc == "/" || typeOfCalc == "остаток от деления" || typeOfCalc == "остаток" || typeOfCalc == "возведение в степень" || typeOfCalc == "^"));
+                    {
+                        Console.WriteLine("Вы не ввели действие. Введите действие или его знак");
+                        typeIsRight = false; //если действие не произведено - цикл необходимо повторить
+                    }
+                } while (typeIsRight == false);
                 Console.WriteLine("x = " + x + "; y = " + y + "; действие - " + typeOfCalc + "; ответ - " + result);
                 Console.WriteLine("Введите слово \"еще\" для повторного вычисления или Enter для завершения программы");
                 doRestart =  Console.ReadLine().ToLower();
