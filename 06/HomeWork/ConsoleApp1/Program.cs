ac07254f7c20f8fe32d11741edcb8ea2e9ea9a0f
Kirill _Catmere_ Rogalskiy <catmere@yandex.ru>
2020-01-25 03:27:21 +0300

    Added HomeWork 6 part 1

---
+++ 06/HomeWork/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите положительное натуральное число: ");
+            string inputString = "";
+            bool mistake = true;
+            int amountEven = 0;
+            while (mistake)
+            {
+                inputString = Console.ReadLine();
+                amountEven = 0;
+                try
+                {
+                    if (inputString == "")
+                        throw new Exception("Число не введено!");
+
+                    foreach (char number in inputString)
+                    {
+                        switch (number.ToString())
+                        {
+                            case "-":
+                                throw new Exception("Число отрицательное!");
+                            case ".":
+                            case ",":
+                                throw new Exception("Число не натуральное!");
+                        }
+
+                        if (int.Parse(number.ToString()) % 2 == 0)
+                            amountEven++;                        
+                    }
+                    mistake = false;
+                }
+                catch (FormatException)
+                {
+                    Console.Write("Введено нечисловое значение! Попробуйте еще раз: ");
+                }
+                catch (Exception e)
+                {
+                    Console.Write(e.Message + " Попробуйте еще раз: ");
+                }
+            }
+            Console.WriteLine($"В числе {inputString} найдено {amountEven} четных чисел");
+            Console.ReadKey();
+        }
+    }
+}
