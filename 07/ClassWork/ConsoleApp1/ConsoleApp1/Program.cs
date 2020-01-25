a2bbafd125c2040bd3ec7f87cdcdd042d272b366
Stas <stanislav_nazarov_97@mail.ru>
2020-01-25 14:07:46 +0300

    Added ClassWork 7

---
+++ 07/ClassWork/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			/*Console.WriteLine("Введите два числа: ");
+			double a = double.Parse(Console.ReadLine());
+			double b = double.Parse(Console.ReadLine());
+			Console.WriteLine(a.ToString("#.##") + " * " + b.ToString("#.##") + " = " + (a * b).ToString("#.##"));
+			Console.WriteLine(string.Format("{0:#.##} + {1:#.##} = {2:#.##}", a, b, a + b));
+			Console.WriteLine($"{a:#.##} - {b:#.##} = {a - b:#.##}");*/
+
+			Console.Write("Введите строку: ");
+			string inputLine = Console.ReadLine();
+			Console.Write("Введите искомую строку: ");
+			string searchedLine = Console.ReadLine();
+			string output = "";
+			int result = 0;
+			do
+			{
+				result = inputLine.IndexOf(searchedLine, result, StringComparison.OrdinalIgnoreCase);
+				if (result == -1)
+					break;
+				output += result + ", ";
+				result++;
+			} while (true);
+			if (output == "")
+				Console.WriteLine("Искомая подстрока не найдена!");
+			else
+				Console.WriteLine("Индекс подстроки в строке: " + output.TrimEnd(',',' '));
+
+		}
+	}
+}
