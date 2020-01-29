136fce5125a9a6fdb6aa4eae577ca68007fadef3
Kirill _Catmere_ Rogalskiy <catmere@yandex.ru>
2020-01-29 17:58:01 +0300

    Added Homework 7

---
+++ 07/HomeWork/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputArray;
+            do
+            {
+                Console.WriteLine("Введите строку из минимум 2 слов:");
+                string input = Console.ReadLine();
+                inputArray = input.Split();
+                if (inputArray.Length < 2)                
+                    Console.WriteLine("Слишком мало слов для обработки! Попробуйте еще раз");                
+            } while (inputArray.Length < 2);
+            int startsWithAAmount = 0;
+            foreach (string word in inputArray)
+            {
+                if (word.StartsWith("а", StringComparison.InvariantCultureIgnoreCase))
+                    startsWithAAmount++;                    
+            }
+            Console.WriteLine($"Количество слов, начинающихся на \'а\': {startsWithAAmount}");
+        }
+    }
+}
---
+++ 07/HomeWork/ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            do
+            {
+                Console.WriteLine("Введите непустую строку: ");
+                input = Console.ReadLine();
+            } while (string.IsNullOrWhiteSpace(input));
+            char[] symbols;
+            symbols = input.ToLower().ToCharArray();
+            string reverseInput = "";
+            for (int i = symbols.Length -1; i >= 0; i--)
+            {
+                reverseInput += symbols[i];
+            }
+            Console.WriteLine(reverseInput);
+        }
+    }
+}
