218c37582f68e85cc2be1d98927e85fa13b9264e
Kirill _Catmere_ Rogalskiy <catmere@yandex.ru>
2020-02-01 09:35:00 +0300

    Homework 8 started

---
+++ 08/HomeWork/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку из скобок такого вида: (, ), [, ]: ");
+            string input = Console.ReadLine();
+            Stack<string> stackOfBrackets = new Stack<string>();
+
+        }
+    }
+}
