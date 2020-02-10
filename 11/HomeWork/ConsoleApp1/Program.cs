03dc9acc2dc72444471f78e9a52286f615ed73a5
Kirill _Catmere_ Rogalskiy <catmere@yandex.ru>
2020-02-11 01:58:41 +0300

    Added HomeWork 11

---
+++ 11/HomeWork/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTimeOffset reminderDateTime;
+            string reminderMessage;
+            Console.Write("Введите количество будильников:");
+            int amountOfReminders = int.Parse(Console.ReadLine());
+            ReminderItem[] reminders = new ReminderItem[2];
+            for (int i = 0; i < 2; i++)
+            {
+                Console.Write($"Введите время будильника №{i + 1}:");
+                try
+                {
+                    reminderDateTime = DateTimeOffset.Parse(Console.ReadLine());
+                    Console.Write($"Введите сообщение будильника №{i + 1}:");
+                    reminderMessage = Console.ReadLine();
+                    reminders[i] = new ReminderItem(reminderMessage, reminderDateTime);
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine($"Ошибка {e.Message}, попробуйте еще раз!");
+                    i--;
+                    continue;
+                }                
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                reminders[i].WriteProperties();
+            }
+
+        }
+    }
+}
---
+++ 11/HomeWork/ConsoleApp1/ReminderItem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class ReminderItem
+    {
+        public DateTimeOffset AlarmDate { get; set; }
+        public string AlarmMessage { get; set; }
+        public TimeSpan TimeToAlarm
+        {
+            get
+            {
+                return AlarmDate - DateTimeOffset.Now;
+            }
+        }
+        public bool IsOutdated
+        {
+            get
+            {
+                bool isOutdated;
+                if (TimeToAlarm > TimeSpan.Zero)
+                    isOutdated = true;
+                else
+                    isOutdated = false;
+                return isOutdated;
+            }
+        }
+        public ReminderItem()
+        {
+
+        }
+        public ReminderItem(string alarmMessage, DateTimeOffset alarmDate)
+        {
+            AlarmDate = alarmDate;
+            AlarmMessage = alarmMessage;
+        }
+        public void WriteProperties()
+        {
+            string outdated = IsOutdated
+                ? "будильник не просрочен"
+                : "будильник просрочен";
+            Console.WriteLine($"Дата и время будильника: {AlarmDate}, текст будильника: {AlarmMessage}, \nвремя до срабатывания будильника: {TimeToAlarm}, {outdated}");
+        }
+    }
+}
