ea49ddff0af504355cd31ab7e908e70c2193fe4b
Kirill _Catmere_ Rogalskiy <catmere@yandex.ru>
2020-02-29 10:32:31 +0300

    Updating my work

---
+++ 13/HomeWork/ConsoleApp1/ConsoleLogWriter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class ConsoleLogWriter : ILogWriter
+    {
+        public ConsoleLogWriter()
+        {
+        }
+        public void LogError(string message)
+        {
+            Console.WriteLine($"{DateTimeOffset.UtcNow:O}: Error: " + message);
+        }
+
+        public void LogInfo(string message)
+        {
+            Console.WriteLine($"{DateTimeOffset.UtcNow:O}: Info: " + message);
+        }
+
+        public void LogWarning(string message)
+        {
+            Console.WriteLine($"{DateTimeOffset.UtcNow:O}: Warning: " + message);
+        }
+    }
+}
--- 13/HomeWork/ConsoleApp1/FileLogWriter.cs
+++ 13/HomeWork/ConsoleApp1/FileLogWriter.cs
@@ -1,28 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace ConsoleApp1
 {
     class FileLogWriter : ILogWriter
     {
-        public FileLogWriter()
-        {
-
-        }
+		private readonly StreamWriter _streamWriter;
+		public string FileName { get; }
+		public FileLogWriter(string fileName)
+		{
+			FileName = fileName;
+			_streamWriter = new StreamWriter(
+				File.Open(
+					fileName,
+				FileMode.Append,
+				FileAccess.Write,
+				FileShare.None));
+		}
+		public void Dispose()
+		{
+			_streamWriter?.Dispose();
+		}
+		
         public void LogError(string message)
         {
-            throw new NotImplementedException();
-        }
+			_streamWriter.WriteLine($"{DateTimeOffset.UtcNow:O}: Error: " + message);
+			_streamWriter.Flush();
+		}
 
         public void LogInfo(string message)
         {
-            throw new NotImplementedException();
-        }
+			_streamWriter.WriteLine($"{DateTimeOffset.UtcNow:O}: Info: " + message);
+			_streamWriter.Flush();
+		}
 
         public void LogWarning(string message)
         {
-            throw new NotImplementedException();
-        }
+			_streamWriter.WriteLine($"{DateTimeOffset.UtcNow:O}: Warning: " + message);
+			_streamWriter.Flush();
+		}
     }
 }
