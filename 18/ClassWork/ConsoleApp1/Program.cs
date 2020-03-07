19b21df0166cb734441ee8284e507718600db55a
Kirill _Catmere_ Rogalskiy <catmere@yandex.ru>
2020-03-07 10:43:44 +0300

    addition of classswork from lesson 18

---
+++ 18/ClassWork/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Reminder.Storage.Core;
+using Reminder.Storage.InMemory;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IReminderStorage storage = new InMemoryReminderStorage();
+            storage.Add(new ReminderItem("Hello World!", 
+                DateTimeOffset.Now,
+                Guid.NewGuid(),
+                "TelegramContactId"               
+                ));
+            List<ReminderItem> list = storage.Get(ReminderItemStatus.Awaiting);
+            foreach(ReminderItem x in list)
+            {
+                Console.WriteLine(x.ToString());
+            }
+        }
+    }
+}
